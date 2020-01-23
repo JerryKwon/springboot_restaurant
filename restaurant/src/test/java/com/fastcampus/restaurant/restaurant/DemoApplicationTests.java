22c0dd821f2690400a742b9d5823c6c18ddc676e
JerryKwon <kwonyoung234@gmail.com>
2020-01-23 18:02:53 +0900

    fixed untracked files

---
+++ restaurant/src/test/java/com/fastcampus/restaurant/restaurant/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.restaurant.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
